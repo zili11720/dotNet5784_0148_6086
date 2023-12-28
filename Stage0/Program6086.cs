5afeae722c3d6c6c96a8e986ba61b25e9894a95a
Ayalag04 <ayala04g@gmail.com>
2023-12-28 18:57:24 +0200

    responce function

---
+++ Stage0/Program6086.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Stage0;
+
+partial class Program
+{
+    static partial void Welcome6086() //seconed user introduction
+    {
+        Console.WriteLine("Hi, I am Ayala");
+    }
+}
