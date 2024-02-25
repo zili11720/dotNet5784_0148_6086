5c6d75d888ee6ce1a273a9dcd2fe04050caaf56e
Ayalag04 <ayala04g@gmail.com>
2024-02-25 16:02:04 +0200

    PL changes

---
+++ PL/Employee/AgentEmployeeWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.Employee
+{
+    /// <summary>
+    /// Interaction logic for AgentEmployeeWindow.xaml
+    /// </summary>
+    public partial class AgentEmployeeWindow : Window
+    {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+        public AgentEmployeeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTaskdDetails_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAvailableTasks_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
