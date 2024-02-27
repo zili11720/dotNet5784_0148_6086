69cc4894933a4f5a2f625f96aee902dc24b0f4af
Ayalag04 <ayala04g@gmail.com>
2024-02-27 02:34:49 +0200

    try

---
+++ BL/BO/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+/// <summary>
+/// A user entity (manager/employee)
+/// </summary>
+/// <param name="UserId">The user's id</param>
+/// <param name="Name">The user's name</param>
+/// <param name="Password">The user's password</param>
+/// <param name="IsManager">A boolien variable that indicats wether the user is a manager or an employee</param>
+/// </summary>
+public class User
+{
+    public required int UserId;
+
+    public required string UserName;
+
+    public required string Password;
+
+    public bool IsManager = false;
+}
