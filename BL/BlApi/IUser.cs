c530b6b54c606934d0412077c0e4d96b0a9b504b
Ayalag04 <ayala04g@gmail.com>
2024-02-27 02:43:29 +0200

    Iuser

---
+++ BL/BlApi/IUser.cs
@@ -0,0 +1,19 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi;
+
+public interface IUser
+{
+
+    string Create(User item);
+    void Delete(int id);
+    User? Read(int id);
+    void Update(User item);
+    void Clear();
+}
+
